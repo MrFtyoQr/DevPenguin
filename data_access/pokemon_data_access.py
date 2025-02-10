fd38cd5f526fc12f7eb363ff8ea1c06339863761
MrFtyoQr <quintana.real.jqr@gmail.com>
2025-02-10 00:55:58 -0600

    joseph/v2.2

--- data_access/pokemon_data_access.py
+++ data_access/pokemon_data_access.py
@@ -2,7 +2,7 @@ class PokemonDataAccess:
     def __init__(self, pokemon_service):
         self.pokemon_service = pokemon_service
 
-    def fetch_pokemon(self, pokemon: str) -> str:
+    def fetch_pokemon(self, pokemon: str) -> str: # del tipo pokemon no
         print("recibimos request de business haremos el request a la infraestructura")
         
         pokemon = self.pokemon_service.get_pokemon(pokemon)
